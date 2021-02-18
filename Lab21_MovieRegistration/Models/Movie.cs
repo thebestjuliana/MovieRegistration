3d7eb60b9e76ca38f3c8dd403b3ea88f8637d2a8
thebestjuliana <76972376+thebestjuliana@users.noreply.github.com>
2021-02-17 19:01:31 -0500

    initial commit

---
+++ Lab21_MovieRegistration/Models/Movie.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab21_MovieRegistration.Models
+{
+
+    public enum Genre
+    {
+        Comedy, 
+        Horror, 
+        Romance, 
+        Kids, 
+        Action, 
+        Indie,
+        Adventure, 
+        Drama,
+        Animation,
+    }
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public Genre Genre { get; set; }
+        public int Year { get; set; }
+        public string[] Actors { get; set; } = new string[4];
+        public string[] Directors { get; set; } = new string[3];
+    }
+}
